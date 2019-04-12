6e6c7c5d81a0e4ebfc4b90c144f82e099ab8100d
thlieh18 <thlieh18@gmail.com>
2019-04-12 17:26:17 +0900

    작성자 : 윤승환

--- Recoder/src/main/java/com/edu/board/vo/BoardVo.java
+++ Recoder/src/main/java/com/edu/board/vo/BoardVo.java
@@ -1,5 +1,93 @@
 package com.edu.board.vo;
 
+import java.util.Date;
+
 public class BoardVo {
 
+	private int boardNo;
+	private int memberNo;
+	private String boardTitle;
+	private String boardContent;
+	private Date boardCreDate;
+	private Date boardModDate;
+	
+	
+	public BoardVo() {
+		super();
+	}
+
+
+	public BoardVo(int boardNo, int memberNo, String boardTitle, String boardContent, Date boardCreDate,
+			Date boardModDate) {
+		super();
+		this.boardNo = boardNo;
+		this.memberNo = memberNo;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+		this.boardCreDate = boardCreDate;
+		this.boardModDate = boardModDate;
+	}
+
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+
+	public int getMemberNo() {
+		return memberNo;
+	}
+
+
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+
+
+	public String getBoardContent() {
+		return boardContent;
+	}
+
+
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+
+
+	public Date getBoardCreDate() {
+		return boardCreDate;
+	}
+
+
+	public void setBoardCreDate(Date boardCreDate) {
+		this.boardCreDate = boardCreDate;
+	}
+
+
+	public Date getBoardModDate() {
+		return boardModDate;
+	}
+
+
+	public void setBoardModDate(Date boardModDate) {
+		this.boardModDate = boardModDate;
+	}
+	
+	
+	
 }
--- Recoder/src/main/java/com/edu/member/vo/MemberVo.java
+++ Recoder/src/main/java/com/edu/member/vo/MemberVo.java
@@ -1,5 +1,70 @@
 package com.edu.member.vo;
 
+import java.util.Date;
+
 public class MemberVo {
 
+	private int memberNo;
+	private char memberAuth;
+	private String memberEmail;
+	private String memberName;
+	private Date memberCreDate;
+	
+	
+	public MemberVo() {
+		super();
+	}
+
+	public MemberVo(int memberNo, char memberAuth, String memberEmail, String memberName, Date memberCreDate) {
+		super();
+		this.memberNo = memberNo;
+		this.memberAuth = memberAuth;
+		this.memberEmail = memberEmail;
+		this.memberName = memberName;
+		this.memberCreDate = memberCreDate;
+	}
+
+	
+	public int getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	public char getMemberAuth() {
+		return memberAuth;
+	}
+
+	public void setMemberAuth(char memberAuth) {
+		this.memberAuth = memberAuth;
+	}
+
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public Date getMemberCreDate() {
+		return memberCreDate;
+	}
+
+	public void setMemberCreDate(Date memberCreDate) {
+		this.memberCreDate = memberCreDate;
+	}
+	
+	
+	
 }
